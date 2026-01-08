e7875fec736a00027b9ca0e3919c726c966d29f7
samarth-sachin <samarthpatil@yahoo.com>
2026-01-08 23:20:31 +0530

    leetcode validaatecoupons

--- Recursion/coordination.java
+++ Recursion/coordination.java
@@ -1,5 +1,5 @@
 public class coordination {
-    public static int countpaths(int i,int j,int n,int m)
+    public static long countpaths(int i,int j,int n,int m)
     {if(i==n||j==m)
         {
             return 0;
@@ -8,14 +8,14 @@ public class coordination {
         {return 1;
         }
     //move downwards
-    int downwards=countpaths(i +1, j, n, m);
+    long downwards=countpaths(i +1, j, n, m);
     //move rights
-    int right=countpaths(i, j+1, n, m);
+    long right=countpaths(i, j+1, n, m);
     return downwards+right;
     }
     public static void main(String[] args) {
-       int m=3;int n=3;
-        int ans=countpaths(0, 0, n, m);
+       int m=21;int n=21;
+        long ans=countpaths(0, 0, n, m);
         System.out.println(ans);
     }
     
--- Solution.java
+++ Solution.java
@@ -1,19 +1,55 @@
-class Solution {
-    public int repeatedNTimes(int[] nums) {
-        int v[]=new int[10001];
-        int n=nums.length/2;
-        for(int i=0;i<nums.length;i++)
-        {
-            v[nums[i]]++;
-        }
-        for(int i=0;i<v.length;i++)
-        {
-            if(v[i]==n) return i;
-        }
-        return -1;
-    }
-    void main()
-    {
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<String> validateCoupons(String[] code, String[] businessLine, boolean[] isActive) {
+        List <String> l=new ArrayList<>();
+        List<String> elec = new ArrayList<>();
+        List<String> groc = new ArrayList<>();
+        List<String> pharm = new ArrayList<>();
+        List<String> rest = new ArrayList<>();
+        boolean v[]=new boolean[code.length];
+        for(int i=0;i<code.length;i++)
+        { int count=0;
+            if (code[i] == null) {
+                v[i] = false;
+                continue;
+            }
+            for (int j = 0; j < code[i].length(); j++) {
+                   char ch = code[i].charAt(j);
+     if ((ch >= 65 && ch <= 90)   
+    || (ch >= 97 && ch <= 122) 
+    || (ch >= 48 && ch <= 57)  
+    || (ch == 95))
+    { count++;
+    }
+}
+ if (code[i].length() > 0 && count == code[i].length()) {
+                v[i] = true;
+            }}
+for(int i=0;i<code.length;i++)
+{
+    if(v[i]!=isActive[i]) v[i]=false;
+}
+for(int i=0;i<businessLine.length;i++)
+{
+     if(businessLine[i].equals("electronics") && v[i]) elec.add(code[i]);
+
+    else if(businessLine[i].equals("grocery") && v[i]) groc.add(code[i]);
+    else if(businessLine[i].equals("pharmacy") && v[i]) pharm.add(code[i]);
+    else if(businessLine[i].equals("restaurant") && v[i]) rest.add(code[i]);
+}
+Collections.sort(elec);
+Collections.sort(groc);
+Collections.sort(pharm);
+Collections.sort(rest);
+l.addAll(elec);
+l.addAll(groc);
+l.addAll(pharm);
+l.addAll(rest);
+
+        return l;
     }
 }
